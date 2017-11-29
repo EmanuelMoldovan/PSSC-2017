3ac2bbb8f08b843aeeb30af77e45fd92c91d7e43
OtiliaOtilia <30649203+OtiliaOtilia@users.noreply.github.com>
2017-11-29 16:38:49 +0200

    Adaugat tema1 si tema2

---
+++ Tiru Otilia/Curs/tema2/laborator.cs/AdapostFry/rasa.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapostFry
+{
+    class rasa
+    {
+        static void Main(string[] args)
+        {
+             public string Nume;
+        public List<Animal.Animal> Listavietati;
+
+        internal rasa(string Nume)
+        {
+            this.Nume = Nume;
+            this.Listavietati = new List<Animal.Animal>();
+        }
+
+        internal rasa(string Nume,List<Animal.Animal> ListaAnimal)
+        {
+            this.Nume = Nume;
+            this.Listavietati = ListaAnimal;
+        }
+
+        public override string ToString()
+        {
+            string sir;
+            sir = " rasa:"+Nume.ToString()+"\n";
+            foreach (Animal.Animal b in Listavietati)
+            {
+                sir += b.ToString();
+            }
+            return sir;
+        }
+    }
+}
