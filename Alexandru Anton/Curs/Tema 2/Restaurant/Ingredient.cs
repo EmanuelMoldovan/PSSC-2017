cf9018a5358e944db8664b44999910954a0a2263
Alexandru95a <32466998+Alexandru95a@users.noreply.github.com>
2017-12-02 21:49:40 +0200

    Tema2
    Added Tema2

---
+++ Alexandru Anton/Curs/Tema 2/Restaurant/Ingredient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+   public class Ingredient
+    {
+        public string nume;
+        public Ingredient (string nume)
+        {
+            this.nume = nume;
+        }
+        public string getNume()
+        {
+            return nume;
+        }
+        public void setNume(string nume)
+        {
+            this.nume = nume;
+        }
+    }
+}
---
+++ Alexandru Anton/Curs/Tema 2/Restaurant/Meniu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public class Meniu
+    {
+         List<Preparate> preparate = new List<Preparate>();
+        public List<Bautura> bauturi = new List<Bautura>();
+          public Meniu(List<Preparate> preparate, List<Bautura> bauturi)
+        {
+            this.preparate = preparate;
+            this.bauturi = bauturi;
+        }
+    }
+}
