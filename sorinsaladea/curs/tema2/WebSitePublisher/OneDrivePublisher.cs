4afd387a933d8fe366d0e311909b5de0a05dfca7
sorinsaladea <sorinsaladea@yahoo.com>
2017-12-13 18:02:28 +0200

    Tema 2

---
+++ sorinsaladea/curs/tema2/WebSitePublisher/OneDrivePublisher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSitePublisher
+{
+    public class OneDrivePublisher
+    {
+        public Uri PublishToOneDrive(string fisierContinut)
+        {
+            Console.WriteLine("Continutul a fost publicat: http://www.onedrive.com/continut.pdf");
+
+            return new Uri("http://www.onedrive.com/continut.pdf");
+        }
+    }
+ }
---
+++ sorinsaladea/curs/tema2/XUnit/Program.cs
@@ -0,0 +1,23 @@
+﻿using External;
+using Modele.Arbitru;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XUnit;
+
+namespace XUnit
+{
+    class Program
+    {
+        private Mock<IExternalItem> _externalItemMock;
+        
+        public void ShouldThrowNullException()
+        {
+            var arbitru = new Arbitru();
+            Assert.Throws<NullReferenceException>(() => arbitru.ReturnMeciuri(arbitru));
+        }
+    }
+}
