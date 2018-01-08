4d70650d7e9f8251fa289a42b3937405746f1522
Emanuel MOLDOVAN <emanuel.moldovan1994@gmail.com>
2018-01-09 00:03:47 +0200

    Proiect FastFood design

---
+++ Moldovan Emanuel/Proiect/FastFood/FastFood/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FastFood.Startup))]
+namespace FastFood
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
