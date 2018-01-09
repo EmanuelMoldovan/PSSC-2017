d7d7cd239ba373b82ccf6001e8376f5bcf20e2bb
Vlad Orbulescu <vladorbulescu10@gmail.com>
2018-01-09 20:51:57 +0200

    Added a form of CQRS (implemented with service command handling), finished testing.

--- ORBULESCU_VLAD/PROIECT/Management Hotelier/MVC/Tests/IdentityAccessTests.cs
+++ ORBULESCU_VLAD/PROIECT/Management Hotelier/MVC/Tests/IdentityAccessTests.cs
@@ -29,7 +29,7 @@ namespace Tests
         [Test]
         [Category("UserManagementShouldPass")]
         [Description("Logging in with correct credentials should work.")]
-        [TestCase("vladorbulescu10","Par0la01")]
+        [TestCase("vladorbulescu10", "Par0la01")]
         public void LoggingInRightCredentialsShouldPass(String u, String p)
         {
             User user = userService.Login(u, p);
@@ -39,22 +39,22 @@ namespace Tests
         [Test]
         [Category("UserManagementShouldFail")]
         [Description("Logging in with wrong credentials should not work.")]
-        [TestCase("asdasda","fsdgdsfsd")]
+        [TestCase("asdasda", "fsdgdsfsd")]
         public void LoggingInWrongCredentialsNotPass(String u, String p)
         {
-            Assert.Throws<WrongCredentialsException>(() => userService.Login(u,p));
+            Assert.Throws<WrongCredentialsException>(() => userService.Login(u, p));
         }
 
         [Test]
         [Category("UserManagementShouldPass")]
-        [Description("Registering a new user should add a new user, verify if it is added, then delete it."+
+        [Description("Registering a new user should add a new user, verify if it is added, then delete it." +
             "from both users and persons storage should work.")]
-        [TestCase("testuser","Testpass1", "test@email.com", "test","test", null, 22)]
+        [TestCase("testuser", "Testpass1", "test@email.com", "test", "test", null, 22)]
         public void RegisterNewUserShouldPass(String username, String pass, String email, String nume, String prenume,
             Adresa a, int varsta)
         {
             User user = userService.Register(username, pass, email, nume, prenume, a, varsta);
-            Assert.AreNotEqual(user,null);
+            Assert.AreNotEqual(user, null);
             Assert.AreEqual(userService.RemoveUser(user.UserId), true);
         }
 
