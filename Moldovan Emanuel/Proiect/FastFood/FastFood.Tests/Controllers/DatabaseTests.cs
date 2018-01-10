4a5ce10f99a32c38909591389dd477a759c6db3e
Radu Petrusan <radu.petrusan@yahoo.com>
2018-01-10 11:38:18 +0200

    modificari Radu

---
+++ Moldovan Emanuel/Proiect/FastFood/FastFood.Tests/Controllers/DatabaseTests.cs
@@ -0,0 +1,27 @@
+﻿using FastFood.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFood.Tests
+{
+    [TestClass]
+    class DatabaseTests
+    {
+        [TestMethod]
+        public void ConnectToDatabase()
+        {
+            //Arrange
+            var model = new Entities();
+
+            //Act
+            var categorie = model.Categorii.FirstOrDefault();
+
+            //Assert
+            Assert.IsNotNull(categorie);
+        }
+    }
+}
--- Moldovan Emanuel/Proiect/FastFood/FastFood.Tests/Controllers/HomeControllerTest.cs
+++ Moldovan Emanuel/Proiect/FastFood/FastFood.Tests/Controllers/HomeControllerTest.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FastFood;
 using FastFood.Controllers;
+using FastFood.Domain;
+using FastFood.Repositories;
 
 namespace FastFood.Tests.Controllers
 {
@@ -49,6 +51,69 @@ namespace FastFood.Tests.Controllers
 
             // Assert
             Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ConnectToDatabase()
+        {
+            //Arrange
+            var model = new Entities();
+
+            //Act
+            var categorie = model.Categorii.FirstOrDefault();
+
+            //Assert
+            Assert.IsNotNull(categorie);
+        }
+
+        [TestMethod]
+        public void AddToDatabase()
+        {
+            //Arrange
+            var model = new Entities();
+            var categorii = model.Categorii.ToList();
+            var initialLength = categorii.Count;
+
+            //Act
+            model.Categorii.Add(new Categorie() { Nume = "test", Poza = "poza" });
+            model.SaveChanges();
+
+            //Assert
+            Assert.AreEqual(initialLength + 1, model.Categorii.ToList().Count);
+        }
+
+        [TestMethod]
+        public void CreateRepository()
+        {
+            //Arrange
+            var repo = new ProduseRepository();
+
+            //Act && Assert
+            Assert.IsNotNull(repo);
+        }
+
+        [TestMethod]
+        public void GetItemsFromRepository()
+        {
+            //Arrange
+            var repo = new ProduseRepository();
+
+            //Act
+            var produse = repo.GetProduse();
+
+            //Assert
+            Assert.IsNotNull(produse);
+        }
+
+        [TestMethod]
+        public void AddProductWithoutPropertiesSetted()
+        {
+            //Arrange
+            var repo = new ProduseRepository();
+            var produs = new Produs();
+
+            //Act && Assert
+            //Assert.ThrowsException(repo.AdaugaProdus(produs, 1));
         }
     }
 }
