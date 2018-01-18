b6b107d439eeba0f7bdb65db82a4b45c2049ea71
AntonCosmin <33652311+AntonCosmin@users.noreply.github.com>
2018-01-18 10:14:54 +0200

    Teme PSSC
    Temele 1 si 2 PSSC

---
+++ Anton Cosmin/Curs/Tema 2/Restaurant/Restaurant.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+     public class Restaurant
+    {
+         Meniu meniu;
+         Orar orar;
+        public int nrMese;
+        public string adresa;
+        public string nume;
+        public Restaurant() { }
+         public Restaurant(Meniu meniu, Orar orar, int nrMese, string adresa, string nume  )
+        {
+            this.meniu = meniu;
+            this.orar = orar;
+            this.nrMese = nrMese;
+            this.adresa = adresa;
+            this.nume = nume;
+        }
+         Meniu getMeniu()
+        {
+            return meniu;
+        }
+       public  Orar getOrar()
+        {
+            return orar;
+        }
+         void setMeniu(Meniu meniu)
+        {
+            this.meniu = meniu;
+        }
+        void setOrar(Orar orar)
+        {
+            this.orar = orar;
+        }
+        public int getNrMese()
+        {
+            return nrMese;
+        }
+        public void setNrMese(int nrMese)
+        {
+            this.nrMese = nrMese;
+        }
+        public string getAdresa()
+        {
+            return adresa;
+        }
+        public void setAdresa(string adresa)
+        {
+            this.adresa = adresa;
+        }
+        public string getNume()
+        {
+            return nume;
+        }
+        public void setNume(string nume)
+        {
+            this.nume = nume;
+        }
+        
+    }
+}
---
+++ Anton Cosmin/Curs/Tema 2/UnitTest/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestaurantBL;
+using Xunit;
+
+namespace UnitTest
+{
+    class Program
+    {
+        public void testAdaugareRestaurant()
+        {
+            RestaurantBL.RestauranteBusinessLogic restaurante = new RestaurantBL.RestauranteBusinessLogic();
+            Restaurant.Restaurant restaurant = new Restaurant.Restaurant();
+            restaurante.AdaugareRestaurante(restaurant);
+            List<Restaurant.Restaurant> test = restaurante.getRestaurante();
+            Assert.Equal(1, test.Count);
+
+        }
+        public void testStergereRestaurant()
+        {
+            RestaurantBL.RestauranteBusinessLogic restaurante = new RestaurantBL.RestauranteBusinessLogic();
+            Restaurant.Restaurant restaurant = new Restaurant.Restaurant();
+            restaurant.setNume("nume1");
+            restaurante.AdaugareRestaurante(restaurant);
+            Restaurant.Restaurant restaurant1 = new Restaurant.Restaurant();
+            restaurant.setNume("nume2");
+            restaurante.AdaugareRestaurante(restaurant1);
+            Restaurant.Restaurant restaurant2 = new Restaurant.Restaurant();
+            restaurant.setNume("nume3");
+            restaurante.AdaugareRestaurante(restaurant2);
+            List<Restaurant.Restaurant> test = restaurante.getRestaurante();
+            Assert.Equal(3, test.Count);
+            Assert.Equal("nume2", test[1].getNume());
+            restaurante.StergereRestaurant("Nume2");
+            List<Restaurant.Restaurant> test1 = restaurante.getRestaurante();
+            Assert.Equal(2, test1.Count);
+            Assert.Equal("nume3", test1[1].getNume());
+        }
+        public void testModificareRestaurant()
+        {
+            RestaurantBL.RestauranteBusinessLogic restaurante = new RestaurantBL.RestauranteBusinessLogic();
+            Restaurant.Restaurant restaurant = new Restaurant.Restaurant();
+            restaurant.setNrMese(5);
+            restaurant.setNume("nume1");
+            restaurante.AdaugareRestaurante(restaurant);
+            Restaurant.Restaurant r = new Restaurant.Restaurant();
+            r.setNrMese(8);
+            restaurant.setNume("nume1");
+            restaurante.ModificareRestaurant(r);
+            List<Restaurant.Restaurant> test = restaurante.getRestaurante();
+            Assert.Equal(test[0].getNrMese(), r.getNrMese());
+
+
+        }
+    }
+}
